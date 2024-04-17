56c68fb6fd17b0a850f7655509b467190e07e0f8
anoopghm <140532297+anoopghm@users.noreply.github.com>
2024-04-17 11:46:07 +0530

    first commit

---
+++ frontend/notes-app/src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Navbar from "../../components/Navbar/Navbar";
+import { useState } from 'react';
+
+const SignUp = () => {
+
+    const[name, setName] = useState("");
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+    const[error, setError] = useState(null);
+
+    const handleSignUp = async(e) => {
+        e.preventDefault();
+    };
+
+    return (
+      <>
+        <Navbar />
+
+        <div className = "flex items-center justify-center mt-28">
+            <div className = "w-96 border rounded bg-white px-7 py-10">
+                <form onSubmit ={handleSignUp}>
+                    <h4 className="text-2xl mb-7">SignUp</h4>
+
+                    <input 
+                    type="text"
+                     placeholder = "Name"
+                     className= "input-box"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    />
+
+                   <input 
+                    type="text"
+                     placeholder = "Email"
+                     className= "input-box"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
+                
+               </form>
+            </div>
+        </div>
+    </>
+ );
+};
+
+export default SignUp;
